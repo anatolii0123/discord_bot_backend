4529ca97d54e8da7b985f651fcaa318abc1fb742
Hunter727 <globalhunter727@gmail.com>
2021-11-12 04:24:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use(session({
 }))
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(cors({
-//     origin: ["https://discord-bot-frontend.herokuapp.com"],
-//     credentials: true
-// }))
 app.use(cors({
-    origin: "*",
+    origin: ["https://discord-bot-frontend.herokuapp.com"],
+    credentials: true
 }))
+// app.use(cors({
+//     origin: "*",
+// }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.get('/test', (req, res) => res.json({ msg: "success" }))
