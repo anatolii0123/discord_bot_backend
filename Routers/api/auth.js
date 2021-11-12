51dd95b8fe715b2d24f8fc4f04553c364b2edc56
SmartDev84 <smartdev@innovdevteam.com>
2021-11-13 01:30:34 +0900

    perfect

--- Routers/api/auth.js
+++ Routers/api/auth.js
@@ -1,8 +1,8 @@
 const app = require('express').Router()
 const passport = require('passport')
 const cfg = require('../../config')
-
 app.get('/discord', passport.authenticate('discord'));
+
 
 app.get('/discord/redirect', passport.authenticate('discord', {
     failureRedirect: `${cfg.FRONDEND_URL}/`
@@ -11,12 +11,13 @@ app.get('/discord/redirect', passport.authenticate('discord', {
     // console.log("session", req)
     // console.log("res", res)
     // res.cookie('user', '2323234')
+    req.session.user = res.user
     res.redirect(`${cfg.FRONDEND_URL}/account`)
 })
 
 app.get('/', (req, res) => {
     console.log("Req", req);
-    if (req.user) {
+    if (req.session.user) {
         console.log("authorized")
         res.json({ msg: "authorized", user: req.user })
     } else {
