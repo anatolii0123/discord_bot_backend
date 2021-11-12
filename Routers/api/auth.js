299c8e7106c3c7f30ba13673a423f3b7aa434c5b
SmartDev84 <smartdev@innovdevteam.com>
2021-11-13 08:36:11 +0900

    Update auth.js

--- Routers/api/auth.js
+++ Routers/api/auth.js
@@ -7,14 +7,14 @@ app.get('/discord', passport.authenticate('discord'));
 app.get('/discord/redirect', passport.authenticate('discord', {
     failureRedirect: `${cfg.FRONDEND_URL}/`
 }), (req, res) => {
-    res.redirect(`${cfg.FRONDEND_URL}/account?user=2222`)
+    res.redirect(`${cfg.FRONDEND_URL}/account?user=${JSON.stringify(res.user)}`)
 })
 
 app.get('/', (req, res) => {
 
     if (req.query.user) {
         console.log("authorized")
-        res.json({ msg: "authorized", user: req.query.user })
+        res.json({ msg: "authorized", user: JSON.parse(req.query.user) })
     } else {
         console.log("unauthorized")
         res.json({ msg: "unauthorized" })
