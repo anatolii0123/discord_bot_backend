65167e014d3842a0eeadac622ca80f6b78cd6a27
Hunter727 <globalhunter727@gmail.com>
2021-11-16 00:00:21 +0900

    https fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token:  {
-        iv:'c46ffbfc969c608d8babda3fadcd90ee',
-        content:'61d1d3b87ddf498f5dcde3c8217481c4ecf75591bec7a2fcd88ab4e2fe7a7f2c830d6958cde94197e075cd6166f76ba60fa0e1e73629f5f5f8d726'
+    token: {
+        iv: 'c46ffbfc969c608d8babda3fadcd90ee',
+        content: '61d1d3b87ddf498f5dcde3c8217481c4ecf75591bec7a2fcd88ab4e2fe7a7f2c830d6958cde94197e075cd6166f76ba60fa0e1e73629f5f5f8d726'
     }, // crypto token from bot
 
 
@@ -11,5 +11,5 @@ module.exports = {
     clientSecret: "vRPq1Z-utnB0tVSfuw3cnSGvysFCylcj", // client secret from bot
     callbackurl: "/api/auth/discord/redirect", // callback
     port: 8000, // port
-    FRONDEND_URL: "https://discord-bot-backend-app.herokuapp.com" // Front-End URL
+    FRONDEND_URL: "http://discord-bot-backend-app.herokuapp.com" // Front-End URL
 }
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-export const BackEnd_URL = 'https://discord-bot-backend-app.herokuapp.com';
-export const FrontEnd_URL = 'https://discord-bot-backend-app.herokuapp.com'
+export const BackEnd_URL = 'http://discord-bot-backend-app.herokuapp.com';
+export const FrontEnd_URL = 'http://discord-bot-backend-app.herokuapp.com'
 export const Name = 'Vexer';
 
 export function getAuth() {
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    return axios.get( `${BackEnd_URL}/api/auth`, { withCredentials: true, credentials: 'include' })
+    return axios.get(`${BackEnd_URL}/api/auth`, { withCredentials: true, credentials: 'include' })
 }
 
 export function getGuilds() {
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    return axios.get( `${BackEnd_URL}/api/discord/guilds`, { withCredentials: true, credentials: 'include' })
+    return axios.get(`${BackEnd_URL}/api/discord/guilds`, { withCredentials: true, credentials: 'include' })
 }
