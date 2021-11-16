b36ac2c539c8a867876384b5683211f73ba55270
Hunter727 <globalhunter727@gmail.com>
2021-11-17 02:00:24 +0900

    init

--- config.js
+++ config.js
@@ -4,7 +4,6 @@ module.exports = {
         content: '61d1d3b87ddf498f5dcde3c8217481c4ecf75591bec7a2fcd88ab4e2fe7a7f2c830d6958cde94197e075cd6166f76ba60fa0e1e73629f5f5f8d726'
     }, // crypto token from bot
 
-
     prefix: "v-", // default prefix
     database: "mongodb+srv://guardian:ASDFasdf1234@cluster0.i86ba.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // link from mongodb
     clientId: "905608828970729482", //client id from bot
--- index.js
+++ index.js
@@ -1,56 +1,54 @@
 console.log("Bot Login Start")
 const { Client, Collection, Intents } = require('discord.js');
-// const fs = require('fs');
-// const { glob } = require("glob");
-// const { dirname } = require('path');
-// const { promisify } = require("util");
+const fs = require('fs');
+const { glob } = require("glob");
+const { dirname } = require('path');
+const { promisify } = require("util");
 const crypto = require('./utils/crypto');
 const config = require('./config')
-// const { connect } = require('./connect')
+const { connect } = require('./bot/src/connect')
 
-// const globPromise = promisify(glob);
+const globPromise = promisify(glob);
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 console.log("token", crypto.decrypt(config.token))
 
-client.on('ready', () => {
-  console.log(`Client Test ${client.user.tag} is logged in!`);
-});
+// client.on('ready', () => {
+//   console.log(`Client Test ${client.user.tag} is logged in!`);
+// });
 
-client.on('messageCreate', (message) => {
-  if (message.content.toLowerCase().includes('fudge') || message.content.toLowerCase().includes('pudding')) {
-    // message.channel.send('Such language is prohibited!');
-    message.reply("Hello, I am hunter!")
-  }
-});
+// client.on('messageCreate', (message) => {
+//   if (message.content.toLowerCase().includes('fudge') || message.content.toLowerCase().includes('pudding')) {
+//     // message.channel.send('Such language is prohibited!');
+//     message.reply("Hello, I am hunter!")
+//   }
+// });
 
-// module.exports = client;
-// client.commands = new Collection();
+module.exports = client;
+client.commands = new Collection();
 
 
 // configs
 
-// connect()
-// const commandFiles = fs.readdirSync(`${__dirname}/commands`).filter(file => file.endsWith('.js '));
-// // -----------------
-// for (const file of commandFiles) {
-//     const command = require(`${__dirname}/commands / $ { file }
-//     `);
-//     client.commands.set(command.name, command);
-//     console.log(` [READY COMMAND] $ { file }
-//     `)
-// }
-// // event handler
-// const eventFiles = fs.readdirSync(`${__dirname}/events`).filter(file => file.endsWith('.js'));
+connect()
+const commandFiles = fs.readdirSync(`${__dirname}/bot/src/commands`).filter(file => file.endsWith('.js '));
+// -----------------
+for (const file of commandFiles) {
+    const command = require(`${__dirname}/bot/src/commands / $ { file } `);
+    client.commands.set(command.name, command);
+    console.log(` [READY COMMAND] $ { file } `)
+}
+// event handler
+const eventFiles = fs.readdirSync(`${__dirname}/bot/src/events`).filter(file => file.endsWith('.js'));
 
-// for (const file of eventFiles) {
-//     const event = require(`${__dirname}/events/${file}`);
-//     if (event.once) {
-//         client.once(event.name, (...args) => event.execute(...args));
-//     } else {
-//         client.on(event.name, (...args) => event.execute(...args));
-//     }
-// }
+for (const file of eventFiles) {
+    const event = require(`${__dirname}/bot/src/events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 require("./server")
 client.login(crypto.decrypt(config.token))
